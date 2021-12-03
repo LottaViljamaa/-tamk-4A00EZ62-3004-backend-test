61ff3f21cbfd6cdadf8d4b10705ede990a935587
LottaViljamaa <lotta.viljamaa@tuni.fi>
2021-12-03 12:19:42 +0200

    Excercise done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+var cors = require("cors");
+app.use(cors());
+const port = process.env.PORT || 8080;
+
+const db = [{ name: "tiina" }, { name: "jack" }];
+
+app.get("/names", (req, res) => {
+  res.send(db);
+});
+
+
+const server = app.listen(port, () => { 
+  console.log(`Listening on port ${server.address().port}`);
+})
